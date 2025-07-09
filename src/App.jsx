6894c1d40685bce83958a235ce4ef5fe6ca13248
Bhagyesh Jethwani <jethwani.bhagyesh@gmail.com>
2025-07-09 00:51:55 -0400

    basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
 const ACCESS_KEY = import.meta.env.VITE_CLASH_ROYALE_TOKEN;
+import { useState } from 'react'
 
 export default function App()
 {
 
+  const [card, updateCard] = useState({
+    name: "",
+    id: "",
+    elixirCost: "",
+    iconUrls: "",
+    rarity: ""
+  })
+
+  return (
+    <div className = "App">
+      <div className = "gallery">
+        <h1> Who have we seen so far?</h1>
+      </div>
+
+      <div className = "card-box"> 
+        <h1> Clash Royale Mayhem</h1>
+        <h2> Discover various Clash Royale Cards!</h2>
+      </div>
+
+      <div className = "ban-list">
+        <h1> Ban List</h1>
+        <h2> Select a type you want to ban!</h2>
+      </div>
+    </div>
+  );
 }
